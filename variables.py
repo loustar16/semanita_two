9e67ddaf5e58001d590a631ee8124248f46151d2
loustar16 <20265134@esen.edu.sv>
2026-09-21 15:32:59 -0600

    trabajando con variables

--- variables.py
+++ variables.py
@@ -1,4 +1,9 @@
 # vamos a aprender variables :3
-print("Hola mundo")
-print("Hoy es miércoles")
-print("Mañana es jueves")
+print("Sistema de mensajería")
+distancia_km = 40.7
+peso_kg = 2.0
+es_urgente = True
+zona = "B"
+
+distancia_km = distancia_km + 16
+print(distancia_km)
